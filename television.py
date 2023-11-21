209286397e60adb91efb02e70a8060472b7bdf0d
calebj635 <130240579+calebj635@users.noreply.github.com>
2023-11-20 18:39:10 -0600

    Fixed television.py

--- television.py
+++ television.py
@@ -15,73 +15,73 @@ class Television:
         :param volume: int to indicate the volume number the tv is set to
         :param channel: int to indicate the channel number the tv is set to
         '''
-        self.status = False
-        self.muted = False
-        self.volume = self.MIN_VOLUME
-        self.channel = self.MIN_CHANNEL
+        self.__status = False
+        self.__muted = False
+        self.__volume = self.MIN_VOLUME
+        self.__channel = self.MIN_CHANNEL
 
     def power(self):
         '''
         Function to switch the Television status to true/false, e.g. turn the tv on/off
         '''
-        if self.status:
-            self.status = False
+        if self.__status:
+            self.__status = False
         else:
-            self.status = True
+            self.__status = True
 
     def mute(self):
         '''
         Function to mute/unmute the tv
         '''
-        if(self.status):
-            if self.muted:
-                self.muted = False
+        if(self.__status):
+            if self.__muted:
+                self.__muted = False
             else:
-                self.muted = True
+                self.__muted = True
 
     def channel_up(self):
         '''
         Function to raise the tv channel by one, or in the case it's on the highest channel, reset it to the lowest
         '''
-        if(self.status):
-            self.channel = self.channel + 1
-            if self.channel > self.MAX_CHANNEL:
-                self.channel = self.MIN_CHANNEL
+        if(self.__status):
+            self.__channel = self.__channel + 1
+            if self.__channel > self.MAX_CHANNEL:
+                self.__channel = self.MIN_CHANNEL
 
     def channel_down(self):
         '''
         Function to lower the tv channel by one, or in the case it's on the lowest channel, reset it to the highest
         '''
-        if(self.status):
-            if (self.channel == self.MIN_CHANNEL):
-                self.channel = self.MAX_CHANNEL
+        if(self.__status):
+            if (self.__channel == self.MIN_CHANNEL):
+                self.__channel = self.MAX_CHANNEL
             else:
-                self.channel -= 1
+                self.__channel -= 1
 
 
     def volume_up(self):
         '''
         Function to raise the volume by one, or in the case its the max volume, keep it the same. Can also unmute if muted.
         '''
-        if(self.status):
-            if(self.muted):
-                self.muted = False
-            if self.volume < self.MAX_VOLUME:
-                self.volume += 1
+        if(self.__status):
+            if(self.__muted):
+                self.__muted = False
+            if self.__volume < self.MAX_VOLUME:
+                self.__volume += 1
                 
     def volume_down(self):
         '''
         Function to lower the volume by one, or in the case its the min volume, keep it the same. Can also unmute if muted.
         '''
-        if(self.status):
-            if(self.muted):
-                self.muted = False
-            if self.volume > self.MIN_VOLUME:
-                self.volume -= 1
+        if(self.__status):
+            if(self.__muted):
+                self.__muted = False
+            if self.__volume > self.MIN_VOLUME:
+                self.__volume -= 1
 
     def __str__(self):
         '''
         Function to return the description of the Television object
         :return: A string detailing the description of Television
         '''
-        return f'Power = [{self.status}], Channel = [{self.channel}], Volume = [{self.volume}]'
+        return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
